add27ace47636bb4e5fa5448e4c5c9a505064031
kris guru <krisbs1122@gmail.com>
2025-01-06 00:12:24 +0530

    changes

---
+++ src/components/Projects/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+<></>
+  )
+}
+
+export default Projects
